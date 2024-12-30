55c57231953d5bbcf431f200d40e28897027e2cf
javier Rival Llanquilef <llanquilef.cl@gmail.com>
2024-12-30 10:12:05 -0300

    Configuración vite lista.

--- frontend/llanqui-page/vite.config.js
+++ frontend/llanqui-page/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegúrate de que el build apunte aquí
+  },
+  server: {
+    host: true,
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
